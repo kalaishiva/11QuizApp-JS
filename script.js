babf3699967780425f64dc8348ec9a8fc485859c
designhappily <65686852+shivakalai@users.noreply.github.com>
2022-09-28 18:06:31 +0200

    Question and answer part generated

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-const quizData = [
-    {
+const quizData = [{
         question: "Which built-in method calls a function for each element in the array?",
         a: "while()",
         b: "loop()",
@@ -33,31 +32,75 @@ const quizData = [
     },
 ];
 
-const quiz;
+/* const quiz;
 const answerEls;
 const questionEl;
 const a_text;
 const b_text;
 const c_text;
 const d_text;
-const submitBtn;
+const submitBtn; */
 
 let currentQuiz = 0
 let score = 0
 
+//question element
+let question = document.getElementById("question");
+// console.log(question);
+
+//identify the length of array
+let randomQuestionLength = quizData.length;
+console.log(randomQuestionLength);
+
+//questions randomly generated
+let randomQuestion = Math.floor(Math.random() * randomQuestionLength)
+console.log(randomQuestion);
+
+let quiz = quizData[randomQuestion];
+// console.log(quiz.question);
+
+question.innerText = quiz.question;
+
+//answer a
+let aAns = document.getElementById("a_text");
+console.log(aAns);
+let aResult = quiz.a;
+aAns.innerText = aResult;
+
+//answer b
+let bAns = document.getElementById("b_text");
+console.log(bAns);
+let bResult = quiz.b;
+bAns.innerText = bResult;
+
+//answer c
+let cAns = document.getElementById("c_text");
+console.log(cAns);
+let cResult = quiz.c;
+cAns.innerText = cResult;
+
+//answer d
+let dAns = document.getElementById("d_text");
+console.log(dAns);
+let dResult = quiz.d;
+dAns.innerText = dResult;
+
+
+
+
+
 loadQuiz()
 
 function loadQuiz() {
-    
+
 }
 
-function deselectAnswers() {
-}
+function deselectAnswers() {}
 
 function getSelected() {
-    
+
 }
 
 submitBtn.addEventListener('click', () => {
-    
+
 })
